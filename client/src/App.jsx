ca00c2505582e1eb28f779c4150a78f1daa5fb6e
kkavinkk <gk.kavin05@gmail.com>
2025-08-20 21:31:43 -0600

    evil twin

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 import Calculator from './components/calculator';
+import main from './components/dataGet'
 
 
 function App() {
   return (
     <div>
+      <main />
       <Calculator />
     </div>
   )
